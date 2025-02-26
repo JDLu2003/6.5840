7f1a57c0ce6ad9b0b1420db27cd9fd509de421c3
JDLu2003 <1277319667@qq.com>
2025-02-26 19:42:18 +0800

    完成实验二，TestReliablePut测试

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -2,10 +2,9 @@ package kvsrv
 
 import (
 	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
-	"6.5840/tester1"
+	kvtest "6.5840/kvtest1"
+	tester "6.5840/tester1"
 )
-
 
 type Clerk struct {
 	clnt   *tester.Clnt
@@ -28,10 +27,21 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
+// Get 用于获取键的当前值和版本。如果键不存在，则返回 ErrNoKey。对于其他所有错误，Get 会持续重试，直到成功为止。
+//
+// 你可以使用如下代码发送 RPC 调用：
+// ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
+//
+// args 和 reply 的类型（包括它们是否为指针）必须与 RPC 处理函数声明的参数类型匹配，并且 reply 必须作为指针传递。
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
-
+	args := rpc.GetArgs{
+		Key: key,
+	}
+	reply := rpc.GetReply{}
+	for !ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply) {
+	}
 	// You will have to modify this function.
-	return "", 0, rpc.ErrNoKey
+	return reply.Value, reply.Version, reply.Err
 }
 
 // Put updates key with value only if version is the version in the
@@ -51,7 +61,26 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
+// 仅当请求中的版本号与服务器端键的版本号匹配时，才使用值更新键。
+// 如果版本号不匹配，服务器应返回 ErrNoVersion。
+// 如果 Put 在第一次 RPC 调用时收到 ErrVersion，Put 应返回 ErrVersion，因为 Put 操作肯定未在服务器端执行。
+// 如果服务器在重发 RPC 时返回 ErrVersion，则 Put 必须向应用程序返回 ErrMaybe，
+// 因为之前的 RPC 可能已被服务器成功处理，但响应丢失，Clerk 无法确定 Put 操作是否已执行。
+//
+// 你可以使用如下代码发送 RPC 调用：
+// ok := ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply)
+//
+// args 和 reply 的类型（包括它们是否为指针）必须与 RPC 处理函数声明的参数类型匹配，并且 reply 必须作为指针传递。
 func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
 	// You will have to modify this function.
-	return rpc.ErrNoKey
+	args := rpc.PutArgs{
+		Key:     key,
+		Value:   value,
+		Version: version,
+	}
+	reply := rpc.PutReply{}
+	for !ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply) {
+	}
+
+	return reply.Err
 }
--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -6,7 +6,7 @@ import (
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
 const Debug = false
@@ -18,15 +18,23 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
-	mu sync.Mutex
+	mu   sync.Mutex
+	data map[string]Inner
 
 	// Your definitions here.
 }
 
+type Inner struct {
+	value   string
+	version rpc.Tversion
+}
+
 func MakeKVServer() *KVServer {
-	kv := &KVServer{}
+	kv := &KVServer{
+		mu:   sync.Mutex{},
+		data: make(map[string]Inner),
+	}
 	// Your code here.
 	return kv
 }
@@ -35,6 +43,17 @@ func MakeKVServer() *KVServer {
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	key := args.Key
+	result, exist := kv.data[key]
+	if exist {
+		reply.Value = result.value
+		reply.Version = result.version
+		reply.Err = rpc.OK
+	} else {
+		reply.Err = rpc.ErrNoKey
+	}
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -43,12 +62,44 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // Args.Version is 0.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	key := args.Key
+	value := args.Value
+	version := args.Version
+	result, exist := kv.data[key]
+	if !exist {
+		// 如果这个key-value不存在
+		if version == 0 {
+			// 如果传入版本为0时创建这个键值对
+			inner := Inner{
+				value:   value,
+				version: 1,
+			}
+			kv.data[key] = inner
+			reply.Err = rpc.OK
+		} else {
+			// 如果传入版本不是0返回ErrNoKey错误
+			reply.Err = rpc.ErrNoKey
+		}
+	} else {
+		// 如果这个key-value存在
+		// 首先判断版本对不对
+		if version == result.version {
+			// 如果版本一致
+			result.version++
+			kv.data[key] = result
+			reply.Err = rpc.OK
+		} else {
+			// 如果版本不一致
+			reply.Err = rpc.ErrVersion
+		}
+	}
 }
 
 // You can ignore Kill() for this lab
 func (kv *KVServer) Kill() {
 }
-
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []tester.IService {
